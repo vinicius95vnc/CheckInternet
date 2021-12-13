2f0e8757ea48862eb348d04b883ea5ce6d544e75
Vinicius Gomes <78374367+vinicius95vnc@users.noreply.github.com>
2021-12-13 15:34:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace CheckInternet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                Console.WriteLine("Status do IP: " + IPStatus.Success);
+                Console.WriteLine("Endereço HOST: " + reply.Address);
+                Console.WriteLine("Tamanho do Buffer: " + reply.Buffer.Length);
+                Console.WriteLine("Tempo de vida: " + reply.Options.Ttl);
+                Console.WriteLine("Tempo execução (ms): " + reply.RoundtripTime);
+                               
+            } catch (Exception)
+            {
+                Console.WriteLine("IP não acessível!");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # CheckInternet
 C# programa que efetua ping de sites 
-
-"...
-                String host = "Neste campo insira o ip ou http para que o programa efetue o ping";
-..."
